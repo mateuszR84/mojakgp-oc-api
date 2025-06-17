5af01970a4afc4657c10830bfe18df79c51f1ae0
Mateusz Rycombel <mateusz@studiodevs.pl>
2025-06-17 22:04:31 +0200

    working on new deploy script cd...

--- plugins/stdevs/kgp/routes.php
+++ plugins/stdevs/kgp/routes.php
@@ -2,7 +2,6 @@
 
 use StDevs\Kgp\Api\Controllers\AuthController;
 use StDevs\Kgp\Api\Controllers\UserController;
-use Illuminate\Auth\Middleware\AuthenticateWithBasicAuth;
 
 // Route::group(['middleware' => AuthenticateWithBasicAuth::class], function() {
 //     Route::post('api/registration', [UserController::class, 'register']);
